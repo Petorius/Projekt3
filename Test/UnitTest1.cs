7686af702f033355f978b3e8fdb1239c52e761f8
Peter von Hohenzollern <petorius@hotmail.com>
2018-10-25 11:19:09 +0200

    Add project files.

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    [TestClass]
+    public class DatabaseTest
+    {
+        [TestMethod]
+        public void TestDatabaseConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+            }
+        }
+    }
+}
