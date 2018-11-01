0758cc502d3423fee55a07833b9e88a3681fac8d
Martin Lavendt Lundgren <mlundg16@student.aau.dk>
2018-11-01 10:44:18 +0100

    Product client domain

---
+++ Client.Domain/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Domain
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public int MinStock { get; set; }
+        public int MaxStock { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+
+        public Product(string name, double price, int stock, int minStock, int maxStock, string description, int rating)
+        {
+            Name = name;
+            Price = price;
+            Stock = stock;
+            MinStock = minStock;
+            MaxStock = maxStock;
+            Description = description;
+            Rating = rating;
+        }
+    }
+}
