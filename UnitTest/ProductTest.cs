2e6cefdcfc064e1808b308e418c5bf99d36e1a21
Morten Albertsen <1067316@ucn.dk>
2018-11-04 13:23:34 +0100

    Test

---
+++ UnitTest/ProductTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest {
+    [TestClass]
+    public class ProductTest {
+
+        [TestMethod]
+        public void InsertTest() {
+
+        }
+
+    }
+}
