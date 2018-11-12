eefc25998aca3a7d82fa52d2f9b594265793e99b
Rune Baun Andersen <1067514@ucn.dk>
2018-11-12 11:27:05 +0100

    Shopping cart design

---
+++ Client.Webshop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Webshop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult ShoppingCart()
+        {
+            ViewBag.Message = "Shopping Cart page";
+
+            return View();
+        }
+    }
+}
