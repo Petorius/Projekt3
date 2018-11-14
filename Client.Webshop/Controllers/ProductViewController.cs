e399e741b883deb44ba6b46182997f2bb84b0259
Martin Lavendt Lundgren <mlundg16@student.aau.dk>
2018-11-14 22:33:48 +0100

    Nyt commit

--- Client.Webshop/Controllers/ProductViewController.cs
+++ Client.Webshop/Controllers/ProductViewController.cs
@@ -1,4 +1,5 @@
-﻿using Client.Domain;
+﻿using Client.ControlLayer;
+using Client.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,17 @@ namespace Client.Webshop.Controllers
 {
     public class ProductViewController : Controller
     {
-        // GET: ProductView
-        public ActionResult Index(int? id)
+        ProductController pc = new ProductController();
+
+
+        public ActionResult Index(int id)
         {
-            ViewBag.Message = "Product view controller ;D";
-            Product p = new Product("Johnny", 200, 1, 1, 1, "LKmasdlkmaslkdmalskdlmsad sakkasdlaksld kasmdkmsd ksmd kask kasm ksadnf jen jsaj jsje jasju sj", null, null, null);
-            return View(p);
+           
+            Product product = pc.Find(id);
+
+            //ViewBag.Message = "Product view controller ;D";
+            //Product p = new Product("Johnny", 200, 1, 1, 1, "LKmasdlkmaslkdmalskdlmsad sakkasdlaksld kasmdkmsd ksmd kask kasm ksadnf jen jsaj jsje jasju sj", null, null, null);
+            return View(product);
         }
     }
 }
