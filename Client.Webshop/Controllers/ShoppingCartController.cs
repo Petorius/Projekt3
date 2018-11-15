56ad1889beadf5db8d7660913b3621ca892da88a
Rune Baun Andersen <1067514@ucn.dk>
2018-11-15 09:27:41 +0100

    add product

--- Client.Webshop/Controllers/ShoppingCartController.cs
+++ Client.Webshop/Controllers/ShoppingCartController.cs
@@ -5,27 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using Client.ControlLayer;
 using Client.Domain;
-using Client.Webshop.Models;
 
-namespace Client.Webshop.Controllers
-{
-    public class ShoppingCartController : Controller
-    {
+namespace Client.Webshop.Controllers {
+    public class ShoppingCartController : Controller {
         private ProductController pc = new ProductController();
         private OrderController oc = new OrderController();
         // GET: ShoppingCart
-        public ActionResult ShoppingCart()
-        {
+        public ActionResult ShoppingCart() {
             ViewBag.Message = "Shopping Cart page";
 
             return View();
         }
 
         [HttpPost]
-        public ActionResult AddProduct(int id, int price, int quantity)
-        {
-            decimal subTotal = price * quantity;
-            oc.CreateOrderLine(quantity, subTotal, id);
+        public ActionResult AddProduct(Product product, Orderline orderline) {
+            orderline.Product = product;
+            decimal subTotal = product.Price * orderline.Quantity;
+            orderline.SubTotal = subTotal;
+            oc.CreateOrderLine(orderline.Quantity, subTotal, product.ID);
 
             return RedirectToAction("Index");
         }
