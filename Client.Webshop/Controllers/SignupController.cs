2cd4b4a431aff96f377b486b4350cf805ae3bac1
Martin Lavendt Lundgren <mlundg16@student.aau.dk>
2018-11-22 15:17:33 +0100

    sign up og login forms

---
+++ Client.Webshop/Controllers/SignupController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Webshop.Controllers
+{
+    public class SignupController : Controller
+    {
+        // GET: Signup
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Signup(string firstName, string lastName, int number, string street, int zip, string city, string email, string password) {
+
+            //Client controller
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
