333ca2d059f4feb5a7b6e65ad39ef23f9489f0d8
Morten Albertsen <1067316@ucn.dk>
2018-11-28 12:21:33 +0100

    Windows ting

---
+++ DesktopClient/CrudOverview.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Client.Domain;
+using Client.ControlLayer;
+
+namespace DesktopClient {
+    /// <summary>
+    /// Interaction logic for CrudOverview.xaml
+    /// </summary>
+
+    public partial class CrudOverview : Window {
+        public string ImageURL { get; set; }
+        public string ImageName { get; set; }
+        private ProductController productController;
+
+        public CrudOverview() {
+            InitializeComponent();
+            productController = new ProductController();
+        }
+
+
+
+        private void Produkt_Opret_OpretProdukt_Clicked(object sender, RoutedEventArgs e) {
+            try {
+                string name = nameTextBox.Text;
+                decimal price = decimal.Parse(priceTextBox.Text);
+                int stock = Int32.Parse(stockTextBox.Text);
+                int minStock = Int32.Parse(minStockTextBox.Text);
+                int maxStock = Int32.Parse(maxStockTextBox.Text);
+                string description = descriptionTextBox.Text;
+                bool res = false;
+                if (ImageName != "" && ImageURL != "") {
+                    res = productController.CreateProduct(name, price, stock, minStock, maxStock, description, ImageURL, ImageName);
+                }
+                if (res) {
+                    addProductText.Content = "Produktet blev tilføjet";
+                    clearFields();
+                }
+                else {
+                    addProductText.Content = "Der skete en fejl";
+                }
+
+
+            }
+            catch (FormatException) {
+                MessageBox.Show("Ugyldig tekst indsat");
+            }
+        }
+
+        private void Produkt_Opret_Fortryd_Clicked(object sender, RoutedEventArgs e) {
+            clearFields();
+        }
+
+        private void clearFields() {
+            nameTextBox.Text = "";
+            priceTextBox.Text = "";
+            stockTextBox.Text = "";
+            minStockTextBox.Text = "";
+            maxStockTextBox.Text = "";
+            descriptionTextBox.Text = "";
+        }
+
+        private void Produkt_Søg_Ok_Clicked(object sender, RoutedEventArgs e) {
+            Product p = productController.Find(Int32.Parse(inputIDtextBox.Text));
+            try {
+                foundNamelabel.Content = p.Name;
+                foundPricelabel.Content = p.Price;
+                foundStocklabel.Content = p.Stock;
+                foundDescriptionlabel.Content = p.Description;
+                foundRatinglabel.Content = p.Rating;
+            }
+            catch (NullReferenceException) {
+                MessageBox.Show("Produktet findes ikke");
+            }
+        }
+
+        private void Produkt_Slet_SletProdukt_Clicked_Click(object sender, RoutedEventArgs e) {
+            int value = Int32.Parse(deleteTextBox.Text);
+            bool res = productController.DeleteProduct(value);
+            if (res == true) {
+                deleteStatusLabel.Content = "Produktet blev slettet";
+                deleteTextBox.Text = "";
+            }
+            else {
+                deleteStatusLabel.Content = "Der opstod en fejl. Prøv igen";
+            }
+
+        }
+
+        private void Søgbutton_Click(object sender, RoutedEventArgs e) {
+            Product p = productController.Find(Int32.Parse(_inputIDtextBox.Text));
+            try {
+                IsActiveButton.IsChecked = p.IsActive;
+                updateNameTextBox.Text = p.Name;
+                updatePriceTextBox.Text = p.Price.ToString();
+                updateStockTextBox.Text = p.Stock.ToString();
+                updateDescriptionTextBox.Text = p.Description;
+                updateMinStockTextBox.Text = p.MinStock.ToString();
+                updateMaxStockTextBox.Text = p.MaxStock.ToString();
+                _inputIDtextBox.IsEnabled = false;
+            }
+            catch (NullReferenceException) {
+                MessageBox.Show("Produktet findes ikke");
+            }
+        }
+
+        private void OKUpdateButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                bool isActive = (bool)IsActiveButton.IsChecked;
+                int id = Int32.Parse(_inputIDtextBox.Text);
+                string name = updateNameTextBox.Text;
+                decimal price = decimal.Parse(updatePriceTextBox.Text);
+                int stock = Int32.Parse(updateStockTextBox.Text);
+                int minStock = Int32.Parse(updateMinStockTextBox.Text);
+                int maxStock = Int32.Parse(updateMaxStockTextBox.Text);
+                string description = updateDescriptionTextBox.Text;
+                bool isUpdated = productController.Update(id, name, price, stock, minStock, maxStock, description, isActive);
+                if (isUpdated) {
+                    updateProductText.Content = "Produktet blev opdateret";
+                    ProductClearUpdateFields();
+                    _inputIDtextBox.IsEnabled = true;
+                }
+                else {
+                    updateProductText.Content = "Der opstod en fejl. Prøv igen";
+                }
+
+            }
+            catch (FormatException) {
+                MessageBox.Show("Ugyldig tekst indsat");
+            }
+        }
+
+        private void ProductClearUpdateFields() {
+            IsActiveButton.IsChecked = false;
+            _inputIDtextBox.Text = "";
+            updateNameTextBox.Text = "";
+            updatePriceTextBox.Text = "";
+            updateStockTextBox.Text = "";
+            updateMaxStockTextBox.Text = "";
+            updateMinStockTextBox.Text = "";
+            updateDescriptionTextBox.Text = "";
+        }
+
+        private void CancelUpdateButton_Click(object sender, RoutedEventArgs e) {
+            _inputIDtextBox.IsEnabled = true;
+            ProductClearUpdateFields();
+        }
+
+        private void AddImageButton_Click(object sender, RoutedEventArgs e) {
+            AddImagesWindow addImagesWindow = new AddImagesWindow();
+            addImagesWindow.Show();
+        }
+    }
+}
