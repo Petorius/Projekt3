edd2476cec9718f6a1e3559fef626744ae275cf5
Rune Baun Andersen <1067514@ucn.dk>
2018-12-03 11:10:36 +0100

    Review test - done

--- UnitTest/ReviewTest.cs
+++ UnitTest/ReviewTest.cs
@@ -13,6 +13,7 @@ namespace UnitTest {
         private UserLogic userLogic;
         private UserDB userDB;
         private ProductDB productDB;
+        private User user;
 
         [TestInitialize]
         public void SetUp() {
@@ -20,7 +21,7 @@ namespace UnitTest {
             userLogic = new UserLogic(connectionString);
             userDB = new UserDB(connectionString);
             productDB = new ProductDB(connectionString);
-            User user = userDB.GetUser("g-star-raw@gmail.gcom");
+            user = userDB.GetUser("g-star-raw@gmail.gcom");
 
             if(user.ID < 1) {
                 userLogic.CreateUserWithPassword("Rune", "G", "G-Street", 9000, "G-Borg", 
@@ -32,7 +33,7 @@ namespace UnitTest {
         public void CreateReview() {
             Review review = new Review();
             Product product = productDB.Get(1);
-            User user = userDB.GetUser("g-star-raw@gmail.gcom");
+            review.Text = "Testing 123 testing";
 
             bool isCreated = reviewDB.CreateReview(review, product.ID, user.ID);
 
@@ -42,11 +43,13 @@ namespace UnitTest {
         [TestMethod]
         public void DeleteReviewExpectedToFail() {
             Review review = new Review();
+            review.Text = "123123";
+            review.User = user;
             Product product = productDB.Get(1);
-            User user = userDB.GetUser("g-star-raw@gmail.gcom");
-            reviewDB.CreateReview(review, product.ID, user.ID, true, true);
 
-            bool isDeleted = reviewDB.DeleteReview(review, true, false);
+            reviewDB.CreateReview(review, product.ID, user.ID);
+
+            bool isDeleted = reviewDB.DeleteReview(review, true, true);
 
             Assert.AreEqual(false, isDeleted);
         }
@@ -54,13 +57,13 @@ namespace UnitTest {
         [TestMethod]
         public void DeleteReview() {
             Review review = new Review();
+            review.Text = "123123";
+            review.User = user;
             Product product = productDB.Get(1);
-            User user = userDB.GetUser("g-star-raw@gmail.gcom");
-            reviewDB.CreateReview(review, product.ID, user.ID, true, true);
 
-            bool isDeleted = reviewDB.DeleteReview(review, true, true);
+            reviewDB.CreateReview(review, product.ID, user.ID);
 
-            Assert.AreEqual(true, isDeleted);
+            bool isDeleted = reviewDB.DeleteReview(review, true);
         }
     }
 }
