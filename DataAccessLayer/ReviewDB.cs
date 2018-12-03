5c3cae9aa81217a9d55439f99f99e94313c24c29
Rune Baun Andersen <1067514@ucn.dk>
2018-12-03 10:55:17 +0100

    Review test - not done

--- DataAccessLayer/ReviewDB.cs
+++ DataAccessLayer/ReviewDB.cs
@@ -1,12 +1,7 @@
-﻿using Server.DataAccessLayer;
+﻿    using Server.DataAccessLayer;
 using Server.Domain;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessLayer {
     public class ReviewDB : IReview {
@@ -16,7 +11,12 @@ namespace DataAccessLayer {
             connectionString = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
         }
 
-        public bool CreateReview(Review review, int productID, int userID) {
+        // Used for unit testing
+        public ReviewDB(string connectionString) {
+            this.connectionString = connectionString;
+        }
+
+        public bool CreateReview(Review review, int productID, int userID, bool test = false, bool testResult = false) {
             bool isCompleted = false;
 
             using (SqlConnection connection = new SqlConnection(connectionString)) {
@@ -38,26 +38,53 @@ namespace DataAccessLayer {
             return isCompleted;
         }
 
-        public bool DeleteReview(Review review) {
+        public bool DeleteReview(Review review, bool test = false, bool testResult = false) {
             bool isDeleted = false;
-            using (SqlConnection connection = new SqlConnection(connectionString)) {
-                connection.Open();
-                using (SqlCommand cmd = connection.CreateCommand()) {
-                    try {
-                        cmd.CommandText = "Delete from Review where reviewID = @reviewID and review.userID = @userID";
-                        cmd.Parameters.AddWithValue("reviewID", review.ID);
-                        cmd.Parameters.AddWithValue("userID", review.User.ID);
-                        cmd.ExecuteNonQuery();
-                        isDeleted = true;
-                    }
-                    catch (SqlException) {
-                        return isDeleted;
+            for (int i = 0; i < 5; i++) {
+                using (SqlConnection connection = new SqlConnection(connectionString)) {
+                    connection.Open();
+                    using (SqlTransaction transaction = connection.BeginTransaction()) {
+                        byte[] rowID = null;
+                        int rowCount = 0;
+                        using (SqlCommand cmd = connection.CreateCommand()) {
+                            try {
+                                cmd.Transaction = transaction;
+                                cmd.CommandText = "SELECT rowID from Review WHERE reviewID = @reviewID";
+                                cmd.Parameters.AddWithValue("reviewID", review.ID);
+                                SqlDataReader reader = cmd.ExecuteReader();
+
+                                while (reader.Read()) {
+                                    rowID = (byte[])reader["rowId"];
+                                }
+                                reader.Close();
+
+                                cmd.CommandText = "Delete from Review where reviewID = @reviewID and review.userID = @userID";
+                                cmd.Parameters.AddWithValue("reviewID", review.ID);
+                                cmd.Parameters.AddWithValue("userID", review.User.ID);
+                                cmd.ExecuteNonQuery();
+                                isDeleted = true;
+
+                                if (test) {
+                                    rowCount = testResult ? 1 : 0;
+                                }
+
+                                if (rowCount == 0) {
+                                    cmd.Transaction.Rollback();
+                                }
+                                else {
+                                    isDeleted = true;
+                                    cmd.Transaction.Commit();
+                                    break;
+                                }
+                            }
+                            catch (SqlException) {
+                                isDeleted = false;
+                            }
+                        }
                     }
                 }
             }
             return isDeleted;
         }
-
-
     }
 }
